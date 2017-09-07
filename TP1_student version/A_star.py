5b7d5b7c1435b133e7aec1c3bd0cc6bcdfef7e29
Olivier Sirois <olivier@debian.local>
2017-09-07 16:15:38 -0400

    errors in main() from A_start, will work on it later

--- TP1_student version/A_star.py
+++ TP1_student version/A_star.py
@@ -3,24 +3,85 @@ import queue as Q
 from Graph import Graph
 import Kruskal
 from Solution import Solution
+import numpy as np
+from numpy import inf
+import copy
 
 SOURCE = 0
 
 class Node(object):
     def __init__(self, v, sol, heuristic_cost=0):
-        self.v = [0]
+        self.v = v
         self.solution = sol
         self.heuristic_cost = None
 
-    def explore_node(self, heap):
-        raise NotImplementedError()
+    def explore_node(self):
+        #Returns all possible nodes connected to this one in a form of a list
+        NextCities = self.solution.edgecosts[self.v, :]
+        NextNodes = []
+        for i in range(0, len(NextCities)):
+            NextNodes.append(copy.deepcopy(Node(self.v, self.solution, self.heuristic_cost)))
+            NextNodes[i].solution.add_edge(NextNodes[i].v,i)
+            NextNodes[i].v = i
+            #print(NextNodes[i].solution.print())
+            print('v=',NextNodes[i].v)
+        return NextNodes 
 
-
+        '''
+        allcities = self.solution.edgecosts[self.v,:]
+        possibilityvector = np.delete(allcities, self.solution.visited)
+        possibilityvector = np.delete(possibilityvector, 0)
+        try:
+            NextDestinationCost = np.ndarray.min(possibilityvector)
+        except ValueError:
+            NextDestinationCost = self.solution.edgecosts[self.v,0]
+            NextCity = 0
+        else:
+            NextDestinationCost = np.ndarray.min(possibilityvector)
+            NextCity = int(np.argwhere(allcities == NextDestinationCost))
+        self.solution.add_edge(self.v, NextCity)
+        self.v = NextCity
+        '''    
+            
 def main():
-    g = Graph("N15.data")
-    import Kruskal
-    Kruskal.kruskal = Kruskal.Kruskal(g)
-    heap = Q.PriorityQueue()
+    g = Graph("N10.data")
+    sol = Solution(g)
+    Nstart = Node(0, sol)
+    N1 = Nstart.explore_node()
+    #N1[7].solution.print()
+    
+    Nusing = copy.deepcopy(Nstart)
+
+    ''''''
+    for i in range(0, g.N):
+        Nnext = Nusing.explore_node()
+        nextcost = []
+        for j in range(0, len(Nnext)):
+            nextcost.append(Nnext[j].solution.cost)
+            print(nextcost[j])
+        #THIS IS WHERE ADD THE HEURISTIC COST
+
+        ##
+        a = Nnext[0].solution.cost
+        for j in range(0, len(Nnext)):
+            print('not_visited',Nnext[j].solution.not_visited)
+            print('j', j)
+            #if (j in Nnext[j].solution.not_visited):
+            print('cost', Nnext[j].solution.cost)
+            print('a', a)
+            if (a > Nnext[j].solution.cost):
+    
+                a = Nnext[j].solution.cost
+                #print("a = ", a)
+                index = j
+                    #print("j = ", j)
+        #nextnode = np.argwhere(np.amin(nextcost) == nextcost)
+
+        Nusing = copy.deepcopy(Nnext[index])
+        #Nusing.solution.print()
+        
+
+    
 
 
 
@@ -30,3 +91,6 @@ def isN2betterThanN1(N1, N2):
 
 if __name__ == '__main__':
     main()
+'''import Kruskal
+    Kruskal.kruskal = Kruskal.Kruskal(g)
+    heap = Q.PriorityQueue()'''
--- TP1_student version/Solution.py
+++ TP1_student version/Solution.py
@@ -3,6 +3,7 @@ import copy
 from Graph import Graph
 from Graph import Edge
 import numpy as np
+from numpy import inf
 
 class Solution(object):
     def __init__(self, s):
@@ -12,28 +13,39 @@ class Solution(object):
             self.visited = []
             self.not_visited = np.arange(0,s.N)
             self.edgecosts = s.costs
+            self.edgecosts[self.edgecosts == 0] = inf
+            self.sortedcosts = list(map(lambda x:x.cost, s.get_sorted_edges()))
+            self.sortedsources = list(map(lambda x:x.source, s.get_sorted_edges()))
+            self.sorteddestinations = list(map(lambda x:x.destination, s.get_sorted_edges()))
+            
             
         elif isinstance(s, Solution):
-            self.g = Solution.g
-            self.cost = Solution.cost
-            self.visited = Solution.visited
-            self.not_visited = Solution.not_visited
+            self.g = s.g
+            self.cost = s.cost
+            self.visited = s.visited
+            self.not_visited = s.not_visited
+            self.edgecosts = s.costs
+            self.sortedcosts = s.sortedcosts
+            self.sortedsources = s.sortedsources
+            self.sorteddestination = s.sorteddestination
         else:
             raise ValueError('you should give a graph or a solution')
 
     def add_edge(self, v, u):
         self.cost = self.cost + self.g.get_edge(v,u).cost
         self.visited.append(u)
-        self.not_visited = np.delete(self.not_visited, u)
+        self.not_visited = np.delete(self.not_visited, np.argwhere(self.not_visited == u))
 
-#    def print(self):
-#        raise NotImplementedError()
+    def print(self):
+        print("These are the visited cities in order", self.visited)
+        print("These are the ones left to visit in order", self.not_visited)
+        print("Here is the cost for the trip",self.cost)
 
-First = Graph('N10.data')
+#First = Graph('N10.data')
 
-Sol = Solution(First)
-
-Sol.add_edge(0,7)
+#Sol = Solution(First)
+'''
+#Sol.add_edge(0,7)
 print(Sol.cost)
 print(Sol.visited)
 print(Sol.not_visited)
@@ -46,4 +58,7 @@ print(Sol.edgecosts)
 #print(Sol.g.costs)
 
 #EXAMPLE MAP
-print(list(map(lambda x:x.cost,Sol.g.get_sorted_edges())))
+print(Sol.sortedcosts)
+print(Sol.sortedsources)
+print(Sol.sorteddestination)
+'''
