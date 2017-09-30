fixed heureustic calculation + removed deep copy

TP1_student version/A_star.py
@@ -14,36 +14,8 @@ SOURCE = 0
 class Node(object):
     def __init__(self, v, sol):
         self.v = v
-        self.solution = sol
+        self.solution = Solution(sol)
         self.Kru = Kruskal(sol.g)
-        
-        cityvector = copy.deepcopy(self.solution.visited)
-        cityvector.append(0)
-
-        
-        try:
-            villeproche = np.amin(np.delete(self.solution.edgecosts[self.v,:], cityvector))
-        except ValueError:
-            villeproche = 0
-        else:
-            villeproche = np.amin(np.delete(self.solution.edgecosts[self.v,:], cityvector))
-
-        try:
-            orgtoville = np.amin(np.delete(self.solution.g.costs[0,:], cityvector))
-        except ValueError:
-            orgtoville = 0
-        else:
-            orgtoville = np.amin(np.delete(self.solution.g.costs[0,:], cityvector))
-            
-        self.heuristic_cost = self.Kru.getMSTCost(self.solution) + villeproche + orgtoville
-
-
-        #self.heuristic_cost = self.Kru.getMSTCost(self.solution)
-
-        #villeproche = np.amin(np.delete(self.solution.edgecosts[self.v,:], self.solution.visited))
-        #print(self.solution.g.costs)
-        #orgtoville = np.amin(np.delete(self.solution.g.costs[0, :], self.solution.visited))
-        #print(orgtoville)
         
     def __lt__(self, other):  
         return isN2betterThanN1(other, self)
@@ -52,46 +24,42 @@ class Node(object):
         #Returns all possible nodes connected to this one in a form of a list
         NextNodes = []
         if(len(self.solution.not_visited) == 1):
-            nn = copy.deepcopy(Node(self.v, self.solution))
+            nn = Node(self.v, self.solution)
             nn.solution.add_edge(nn.v,0)
             nn.v = 0
             nn.update_heuristic_cost()
             NextNodes.append(nn)
-            
         else:
             for i in self.solution.not_visited:
                 if i != 0:
-                    nn = copy.deepcopy(Node(self.v, self.solution))
+                    nn = Node(self.v, self.solution)
                     nn.solution.add_edge(nn.v,i)
                     nn.v = i
                     nn.update_heuristic_cost()
                     NextNodes.append(nn)
         return NextNodes
-    def update_heuristic_cost(self):
-        #updates the cost of the heuristic for the Node, used in the explore_node method to update the heuristic cost before returning the new nodes
-        cityvector = copy.deepcopy(self.solution.visited)
-        cityvector.append(0)
-        try:
-            villeproche = np.amin(np.delete(self.solution.edgecosts[self.v,:], cityvector))
-        except ValueError:
-            villeproche = 0
-        else:
-            villeproche = np.amin(np.delete(self.solution.edgecosts[self.v,:], cityvector))
-
-        try:
-            orgtoville = np.amin(np.delete(self.solution.g.costs[0,:], cityvector))
-        except ValueError:
-            orgtoville = 0
-        else:
-            orgtoville = np.amin(np.delete(self.solution.g.costs[0,:], cityvector))
-            
-        self.heuristic_cost = self.Kru.getMSTCost(self.solution) + villeproche + orgtoville
-        #self.heuristic_cost = 0     
-        #print('ville la plus proche', villeproche)
-        #print('closest to origin', orgtoville)
-        #self.solution.print()
-
         
+    def update_heuristic_cost(self):
+        #updates the cost of the heuristic for the Node, used in the explore_node method to update the heuristic cost before returning the new nodes                 
+
+        if(len(self.solution.not_visited) == 0):  
+           villeproche = 0
+           orgtoville = 0
+           mstCost = 0
+        elif(len(self.solution.not_visited) == 1):  
+            villeproche = 0
+            orgtoville = self.solution.g.costs[0, self.v]
+            mstCost = 0
+        else:         
+           costOfNonVistedWithoutZero = self.solution.g.costs[:,np.delete(self.solution.not_visited, 0)]
+           orgtoville = np.amin(costOfNonVistedWithoutZero[0, :])
+           villeproche = np.amin(costOfNonVistedWithoutZero[self.v, :])
+           mstCost = self.Kru.getMSTCost(self.solution) 
+      
+        self.heuristic_cost = 0
+        self.heuristic_cost = self.heuristic_cost + mstCost              
+        self.heuristic_cost = self.heuristic_cost + villeproche
+        self.heuristic_cost = self.heuristic_cost + orgtoville        
         
 def main():
     g = Graph("N15.data")
@@ -124,11 +92,6 @@ def main():
                 #print("adding node at", NewNodes[i].v,"and cost", NewNodes[i].solution.cost + NewNodes[i].heuristic_cost)
                 Nodes.put(NewNodes[i])
 
-        # Nodes should be using a high priority queue not just an array ...        
-        
-        #print(Nodes[len(Nodes)-1].solution.not_visited)
-       
-    #print(Nodes[0].solution.g.costs)
 
 
 def isN2betterThanN1(N1, N2):

TP1_student version/Solution.py
@@ -7,35 +7,29 @@ from numpy import inf
 import copy
 
 class Solution(object):
-    def __init__(self, s):
-        if isinstance(s, Graph):
-            self.g = s
+    def __init__(self, val):
+        if isinstance(val, Graph):
+            self.g = val
             self.cost = 0
-            self.visited = []
-            self.not_visited = np.arange(0,s.N)
-            self.edgecosts = copy.deepcopy(s.costs)
-            self.edgecosts[self.edgecosts == 0] = inf
-            self.edges = []
+            self.visited = np.empty(shape=0, dtype = int)
+            self.not_visited = np.arange(0,val.N)
             self.explored_nodes = 0
             self.created_nodes = 0
-        elif isinstance(s, Solution):
-            self.g = s.g
-            self.cost = s.cost
-            self.visited = s.visited
-            self.not_visited = s.not_visited
-            self.edgecosts = s.edgecosts
-            self.edges = s.edges
-            self.explored_nodes = s.explored_nodes
-            self.created_nodes = s.created_nodes
+        elif isinstance(val, Solution):
+            self.g = val.g
+            self.cost = val.cost
+            self.visited = val.visited
+            self.not_visited = val.not_visited
+            self.explored_nodes = val.explored_nodes
+            self.created_nodes = val.created_nodes
         else:
             raise ValueError('you should give a graph or a solution')
 
     def add_edge(self, v, u):
         #do we need to check if v  is in the end of visited 
         self.cost = self.cost + self.g.get_edge(v,u).cost
-        self.visited.append(u)
+        self.visited = np.append(self.visited, u)
         self.not_visited = np.delete(self.not_visited, np.argwhere(self.not_visited == u))
-        self.edges.append(Edge(v,u, self.g.get_edge(v,u).cost))
 
     def print(self):
         print("These are the visited cities in order", self.visited)
@@ -44,21 +38,3 @@ class Solution(object):
         print("number of explored nodes ",self.explored_nodes)
         print("number of created nodes ",self.created_nodes)
 
-'''
-#Sol.add_edge(0,7)
-print(Sol.cost)
-print(Sol.visited)
-print(Sol.not_visited)
-
-print(Sol.edgecosts)
-#print(Sol.cost)
-
-#print(Sol.visited)
-#print(Sol.not_visited)
-#print(Sol.g.costs)
-
-#EXAMPLE MAP
-print(Sol.sortedcosts)
-print(Sol.sortedsources)
-print(Sol.sorteddestination)
-'''